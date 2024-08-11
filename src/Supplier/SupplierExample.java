871a40493ef9e32d253cc2e460c9b45d72a0bb13
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-11 18:01:41 -0300

    add supplier

---
+++ src/Supplier/SupplierExample.java
@@ -0,0 +1,23 @@
+package Supplier;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Representa uma operação que não aceita nenhum argumento e retorna um resultado do tipo T.
+ * É comumente usada para criar ou fornecer novos objetos de um determinado tipo.
+ */
+
+public class SupplierExample {
+    public static void main(String[] args) {
+
+       // Supplier<String> saudacao = () -> "Olá, seja bem-vindo!";
+
+        List<String> listaSaudacao = Stream.generate(
+                () -> "Olá, seja bem-vindo!")
+                .limit(5)
+                .toList();
+
+        listaSaudacao.forEach(s -> System.out.println(s));
+    }
+}
