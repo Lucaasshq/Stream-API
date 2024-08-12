9005a5d6c4dc38b5549c9676ccccd427a0381ac1
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-12 00:00:41 -0300

    .

--- src/BinaryOperator/BinaryOperatorExample.java
+++ src/BinaryOperator/BinaryOperatorExample.java
@@ -8,11 +8,12 @@ public class BinaryOperatorExample {
 
     public static void main(String[] args) {
 
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        List<Integer> numeros = Arrays.asList(5,10);
 
         BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
 
-        int resultado = numeros.stream().reduce(0, (a, b) -> a + b);
+        int resultado = numeros.stream()
+                .reduce(0, (a, b) -> a + b);
 
         System.out.println(resultado);
     }
